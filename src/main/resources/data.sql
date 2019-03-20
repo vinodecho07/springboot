e5b11c0b96f74fdf4539f82afdb0d3bf7b3d9798
PanaromicHCL <>
2019-03-20 15:52:27 +0530

    data sql added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into test.Product_group (product_group_id, product_group_name, product_group_description) values(1, 'Mortgage', 'Mortgage Group');
+insert into test.Product_group (product_group_id, product_group_name, product_group_description) values(2, 'Payments', 'Payments Group');
+insert into test.Product_group (product_group_id, product_group_name, product_group_description) values(3, 'Savings', 'Savings Group');
+
