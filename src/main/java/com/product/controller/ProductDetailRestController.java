63cdd063552b5a6db8be2366f86e423c4e4da4ae
PanaromicHCL <>
2019-03-20 16:42:25 +0530

    details api updated

--- src/main/java/com/product/controller/ProductDetailRestController.java
+++ src/main/java/com/product/controller/ProductDetailRestController.java
@@ -1,6 +1,8 @@
 package com.product.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,9 +21,9 @@ public class ProductDetailRestController {
 	ProductDetailService productDetailService;
 
 	@GetMapping("/getProductDetail/{productId}")
-	public ProductDetail getProductDetail(@PathVariable Long productId) {
+	public ResponseEntity<ProductDetail> getProductDetail(@PathVariable Long productId) {
 		ProductDetail ProductDetail = productDetailService.getProductDetails(productId);
-		return ProductDetail;
+		return new ResponseEntity<>(ProductDetail, HttpStatus.OK);
 	}
 
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,12 @@
-insert into `ingsuite`.'Product_group' (product_group_id, product_group_name, product_group_description) values(1, 'Mortgage', 'Mortgage Group');
-insert into `ingsuite`.'Product_group' (product_group_id, product_group_name, product_group_description) values(2, 'Payments', 'Payments Group');
-insert into `ingsuite`.'Product_group' (product_group_id, product_group_name, product_group_description) values(3, 'Savings', 'Savings Group');
+insert into ingsuite.Product_group (product_group_id, product_group_name, product_group_description) values(1, 'Mortgage', 'Mortgage Group');
+insert into ingsuite.Product_group (product_group_id, product_group_name, product_group_description) values(2, 'Payments', 'Payments Group');
+insert into ingsuite.Product_group (product_group_id, product_group_name, product_group_description) values(3, 'Savings', 'Savings Group');
 
-INSERT INTO `ingsuite`.`product` (`product_id`, `product_description`, `product_name`, `product_group_id`) VALUES ('1', 'Orange savings account desc', 'Orange savings account', '3');
-INSERT INTO `ingsuite`.`product` (`product_id`, `product_description`, `product_name`, `product_group_id`) VALUES ('2', 'Child savings account desc', 'Child savings account', '3');
-INSERT INTO `ingsuite`.`product` (`product_id`, `product_description`, `product_name`, `product_group_id`) VALUES ('3', 'Savings account for Unicef desc', 'Savings account for Unicef', '3');
-INSERT INTO `ingsuite`.`product` (`product_id`, `product_description`, `product_name`, `product_group_id`) VALUES ('4', 'Bonus interest account desc', 'Bonus interest account', '3');
-INSERT INTO `ingsuite`.`product` (`product_id`, `product_description`, `product_name`, `product_group_id`) VALUES ('5', 'Savings deposit desc', 'Savings deposit', '3');
-INSERT INTO `ingsuite`.`product` (`product_id`, `product_description`, `product_name`, `product_group_id`) VALUES ('6', 'Green savings deposit desc', 'Green savings deposit', '3');
+INSERT INTO ingsuite.product (product_id, product_description, product_name, product_group_id) VALUES ('1', 'Orange savings account desc', 'Orange savings account', '3');
+INSERT INTO ingsuite.product (product_id, product_description, product_name, product_group_id) VALUES ('2', 'Child savings account desc', 'Child savings account', '3');
+INSERT INTO ingsuite.product (product_id, product_description, product_name, product_group_id) VALUES ('3', 'Savings account for Unicef desc', 'Savings account for Unicef', '3');
+INSERT INTO ingsuite.product (product_id, product_description, product_name, product_group_id) VALUES ('4', 'Bonus interest account desc', 'Bonus interest account', '3');
+INSERT INTO ingsuite.product (product_id, product_description, product_name, product_group_id) VALUES ('5', 'Savings deposit desc', 'Savings deposit', '3');
+INSERT INTO ingsuite.product (product_id, product_description, product_name, product_group_id) VALUES ('6', 'Green savings deposit desc', 'Green savings deposit', '3');
+
+INSERT INTO ingsuite.product_detail (product_detail_id, duration, maximum_investment, minimum_investment, precentage_of_interest, rate_of_interest, special, withdrawal, product_product_id) VALUES ('2', 'unlimited', '99999999', '0', '0.20', 'variable', 'Savings Lion as present', 'always possible', '2');
