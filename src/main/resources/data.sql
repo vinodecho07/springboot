82a48962b0056fbec86cb6cda2b86684ad5d24ff
PanaromicHCL <>
2019-03-20 17:22:42 +0530

    sonar added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,17 @@
+insert into ingsuite.Product_group (product_group_id, product_group_name, product_group_description) values(0, 'Overview', 'Overview');
 insert into ingsuite.Product_group (product_group_id, product_group_name, product_group_description) values(1, 'Mortgage', 'Mortgage Group');
 insert into ingsuite.Product_group (product_group_id, product_group_name, product_group_description) values(2, 'Payments', 'Payments Group');
 insert into ingsuite.Product_group (product_group_id, product_group_name, product_group_description) values(3, 'Savings', 'Savings Group');
+
+insert into ingsuite.product (product_id, product_description, product_name, product_group_id) values(1,'Bank Saving mortgage','Bank Saving mortgage',1);
+insert into ingsuite.product (product_id, product_description, product_name, product_group_id) values(2,'Interest-only','Interest-only',1);
+insert into ingsuite.product (product_id, product_description, product_name, product_group_id) values(3,'Annuity','Annuity',1);
+insert into ingsuite.product (product_id, product_description, product_name, product_group_id) values(4,'Linear','Linear',1);
+
+insert into ingsuite.product (product_id, product_description, product_name, product_group_id)(product_id, product_name, product_description, product_group_id) values(5,'Payments account','Payments account',2);
+insert into ingsuite.product (product_id, product_description, product_name, product_group_id) values(6,'Debit card','Debit card',2);
+insert into ingsuite.product (product_id, product_description, product_name, product_group_id) values(7,'Quarterly limit','Quarterly limit',2);
+insert into ingsuite.product (product_id, product_description, product_name, product_group_id) values(8,'Continuously limit','Continuously limit',2);
 
 INSERT INTO ingsuite.product (product_id, product_description, product_name, product_group_id) VALUES ('1', 'Orange savings account desc', 'Orange savings account', '3');
 INSERT INTO ingsuite.product (product_id, product_description, product_name, product_group_id) VALUES ('2', 'Child savings account desc', 'Child savings account', '3');
@@ -10,3 +21,4 @@ INSERT INTO ingsuite.product (product_id, product_description, product_name, pro
 INSERT INTO ingsuite.product (product_id, product_description, product_name, product_group_id) VALUES ('6', 'Green savings deposit desc', 'Green savings deposit', '3');
 
 INSERT INTO ingsuite.product_detail (product_detail_id, duration, maximum_investment, minimum_investment, precentage_of_interest, rate_of_interest, special, withdrawal, product_product_id) VALUES ('2', 'unlimited', '99999999', '0', '0.20', 'variable', 'Savings Lion as present', 'always possible', '2');
+
