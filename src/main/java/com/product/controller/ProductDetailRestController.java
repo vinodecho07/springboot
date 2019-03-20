36f8364316d92520f402fdcc6a67f34bfe2e8224
PanaromicHCL <>
2019-03-20 15:59:31 +0530

    details api added

---
+++ src/main/java/com/product/controller/ProductDetailRestController.java
@@ -0,0 +1,29 @@
+package com.product.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.product.model.ProductDetail;
+import com.product.service.ProductDetailService;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ProductDetailRestController {
+
+	@Autowired
+	ProductDetailService productDetailService;
+
+	@GetMapping("/getProductDetail/{productId}")
+	public ProductDetail getProductDetail(@PathVariable Long productId) {
+		ProductDetail ProductDetail = productDetailService.getProductDetails(productId);
+		return ProductDetail;
+	}
+
+
+
+}
