91025eaf4e3b2273513e7fec94e0534a2817366d
PanaromicHCL <>
2019-03-20 16:19:50 +0530

    database changes

--- src/main/java/com/product/model/ProductDetail.java
+++ src/main/java/com/product/model/ProductDetail.java
@@ -1,5 +1,7 @@
 package com.product.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,7 +18,7 @@ import lombok.ToString;
 @Entity
 @Table
 @Setter @Getter @NoArgsConstructor @ToString
-public class ProductDetail {
+public class ProductDetail implements Serializable{
 	
 	/**
 	 *
--- src/main/java/com/product/service/ProductDetailServiceImpl.java
+++ src/main/java/com/product/service/ProductDetailServiceImpl.java
@@ -1,10 +1,12 @@
 package com.product.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.product.model.ProductDetail;
 import com.product.repository.ProductDetailRepository;
 
+@Service
 public class ProductDetailServiceImpl implements ProductDetailService {
 	
 	@Autowired
